58bdaf071a30d79b26289e593cd7f99ce8fc7570
mativiscusso <mativiscusso@gmail.com>
2021-08-10 11:21:22 -0300

    sadsa

--- utils/helpers/index.js
+++ utils/helpers/index.js
@@ -1,34 +1,3 @@
-export const listToTree2 = (data, options) => {
-    options = options || {}
-    const ID_KEY = options.idKey || 'id'
-    const PARENT_KEY = options.parentKey || 'parent'
-    const CHILDREN_KEY = options.childrenKey || 'children'
-
-    const tree = []
-    const childrenOf = {}
-    let item, id, parentId
-
-    for (let i = 0, length = data.length; i < length; i++) {
-        item = data[i]
-        id = item[ID_KEY]
-        parentId = item[PARENT_KEY].id || 0
-        // every item may have children
-        childrenOf[id] = childrenOf[id] || []
-        // init its children
-        item[CHILDREN_KEY] = childrenOf[id]
-        if (parentId !== 1) {
-            // init its parent's children object
-            childrenOf[parentId] = childrenOf[parentId] || []
-            // push it into its parent's children object
-            childrenOf[parentId].push(item)
-        } else {
-            tree.push(item)
-        }
-    }
-
-    return tree
-}
-
 export const listToTree = (nodes) => {
     const topLevelNodes = []
     const mappedArr = {}
@@ -81,6 +50,7 @@ export const formatFacetValues = (facetValues) => {
     }
     return facets
 }
+
 const facetSelectedFormated = (facetValues, facetSelected) => {
     let result
     facetValues.forEach(({ values }) => {
